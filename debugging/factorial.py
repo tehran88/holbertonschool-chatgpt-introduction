c8ab872d20f3980ca521198c2f34aca0cfc9cdb1
tehran88 <tehranabilovaa@gmail.com>
2025-01-11 15:37:14 +0400

    factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -4,10 +4,9 @@ import sys
 def factorial(n):
     result = 1
     while n > 1:
-        result *= n
-        n -= 1
+        result = result * n
+        n = n - 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
-
+newf = factorial(int(sys.argv[1]))
+print(newf)
